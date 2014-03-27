ad25df48eb5a542f8cd5dcea21014f6cc1c61697
Nick Douma <n.douma@nekoconeko.nl>
2014-03-27 13:53:37 +0100

    Add logic up until configuration loading

--- munin2es.py
+++ munin2es.py
@@ -18,13 +18,53 @@
 # along with munin2es. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import sys, signal, time, setproctitle
+
+setproctitle.setproctitle("munin2es")
+
 import logging
+from argparse import ArgumentParser
+import munin2es
 from munin2es import process_munin_client
+from chaos.arguments import get_config_arguments
 from chaos.logging import get_logger
 
-## Initialize root logger with handlers
+## Initialize root logger with default handlers
+get_logger(None, level=logging.INFO, handlers={"syslog": None, "console": None})
+
+(config_arg, config_unknown) = get_config_arguments()
+
+print config_arg
+print config_unknown
+
+log_handlers = {}
+if not config_arg.version and not config_arg.help:
+	log_handlers['syslog'] = None
+loglevel = logging.INFO
+if config_arg.verbose:
+	loglevel = logging.DEBUG
+if not config_arg.quiet:
+	log_handlers['console'] = None
+
+## Reset the root logger with the new values
 ## All other loggers will propagate their events to the root logger, and use
 ## its handlers and settings.
-get_logger(None, level=logging.DEBUG, handlers={"syslog": None, "console": None})
+get_logger(None, level=loglevel, handlers=log_handlers)
 
-process_munin_client(node="localhost", port=4949)
+## Get the actual logger we will use
+logger = get_logger(munin2es.NAME)
+
+if config_arg.version:
+	logger.info("{0} version {1} ({2})".format(munin2es.NAME, munin2es.VERSION, munin2es.BUILD))
+	sys.exit(0)
+
+if not config_arg.help:
+	logger.info("{0} version {1} ({2}) starting...".format(munin2es.NAME, munin2es.VERSION, munin2es.BUILD))
+
+munin2es.STARTARG = config_arg
+
+munin2es.reload_config()
+print munin2es.HOSTDIR
+print munin2es.WORKERS
+
+#process_munin_client(node="localhost", port=4949)
